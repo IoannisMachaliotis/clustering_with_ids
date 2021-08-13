d60fea75b511a1c019bc6dcb643a168baf328122
Ioannis Machaliotis <80756418+IoannisMachaliotis@users.noreply.github.com>
2021-08-13 13:27:54 +0300

    Update test_aeclustering.cpp

--- src/test_aeclustering.cpp
+++ src/test_aeclustering.cpp
@@ -1,3 +1,9 @@
+/* Code that prints all the visualized clusters from opencv (green clusters)
+   to the terminal 
+
+   The goal is to save also previous moment clusters
+*/
+
 #include <ros/callback_queue.h>
 #include <iostream>
 #include <cstdio>
@@ -10,16 +16,17 @@
 #include <sensor_msgs/Image.h>
 #include <dvs_msgs/EventArray.h>
 #include "grvc_e_clustering/AEClustering.h"
-
+#include <vector>
+#include <math.h>
 
 AEClustering *eclustering(new AEClustering);
 
 
 image_transport::Publisher pubIm;
 sensor_msgs::ImagePtr im_msg;
-//next step is to make the bellow arrays dynamic so that i can delete line 113
-int cluster_centers[70][2];
-int previous_clusters[70][2];
+
+std::vector<std::vector<int>> cluster_centers; // Prosoxi ta vectors einai 2 diastasewn!!
+std::vector<std::vector<int>> previous_clusters;
 int new_count;
 int previous_count;
 int dif;
@@ -27,101 +34,97 @@ int dif;
 void imageCallback(const sensor_msgs::ImageConstPtr& msg){
     try{
         int Id=1;// Create Cluster ID
-
-        //Initialize clusters array so that I can count the elements after
-        for (int i=0; i<sizeof(cluster_centers)/sizeof(cluster_centers[0]); i++){
-                for (int j=0; j<=1;j++ ){
-                        cluster_centers[i][j]=0;
-                }
-        }
-
         int minN = eclustering->getMinN();// 15
         cv::Mat im = cv_bridge::toCvShare(msg, "rgb8")->image;
         std::cout << "NEW CLUSTERS..\n";
+
+/*               
+                // SAVE PREVIOUS CLUSTERS
+                for (std::vector<int> n : cluster_centers) {
+                    previous_clusters.push_back(n);
+                }
+*/
         for (auto cc : eclustering->clusters){
+
             if (cc.getN() >= minN){//αν ο αριθμος των δεδομενων που συνεβαλαν για να δημιουργηθει ο cluster ειναι μεγαλυτερος του 15
+
+                // Erase clusters for update
+                cluster_centers.erase(cluster_centers.begin(), cluster_centers.end());
+
                 Eigen::VectorXd cen(cc.getClusterCentroid());
-//              cv::circle(im,cv::Point(cen[0],cen[1]), 2, cv::Scalar(0,255,0,255), -1);
 
-              if (Id<80){ //choose which Ids to see
+                //Convert cen vector from eigen vector to std::vector
+                std::vector<int> cen_converted(&cen[0], cen.data()+cen.cols()*cen.rows());
 
-                //save new cluster center, generated with ID, in a list
-                for (int j=0; j<=1; j++){
-                      cluster_centers[Id][j]=cen[j];
-                        cv::circle(im,cv::Point(cen[0],cen[1]), 2, cv::Scalar(0,255,0,255), -1);
+                if (Id<80){ //choose which Ids to see
+                    cv::circle(im,cv::Point(cen[0],cen[1]), 2, cv::Scalar(0,255,0,255), -1);
+
+                    //save new cluster center, generated with ID, in a VECTOR
+                    cluster_centers.push_back(cen_converted);
+
+                    //SHOW NEW CLUSTERS
+                    //show ID and center of new clusters
+                    std::cout << "ID:"<< Id << "   Center: ";
+                    for (std::vector<int> n : cluster_centers){
+                        int j=0;
+                        int k=0;
+                        for ( int i : n) {
+                            std::cout << i;
+                            if (k%2==0){std::cout << ",";}
+                            k++;
+                        }
+                        j++;
+                        if (j%2==0) {
+                            std::cout << " \n";
+                        }
+                    }
+                    std::cout << "\n";
+
+                    Id++;
                 }
-
-                //show ID and center of new clusters
-                std::cout << "ID:"<< Id << "   Center:";
-                for (int j=0; j<=1; j++){
-                        std::cout << cluster_centers[Id][j];
-                        if (j==0) {std::cout << ",";}
-                        std::cout << " ";
-                }
-                std::cout << "\n";
-              }
-                Id++;
-                new_count=0;
             }
         }
+        /*
+                    //SHOW PREVIOUS CLUSTERS
+//                    std::cout << "                           PREVIOUS CLUSTERS..\n";
+                    int m=1;
+                    for (std::vector<int> n : previous_clusters){
+                        int j=0;
+
+                        //show ID and center of previous clusters
+                        std::cout << "                          ID:"<< m << "   Center: ";
+                        int k=0;
+                        for ( int i : n) {
+                            std::cout << i;
+                            if (k%2==0){std::cout << ",";}
+                            k++;
+                        }
+                        j++;
+                        if (j%2==0) {
+                            std::cout << "\n";
+                        }
+                        m++;
+                    }
+                    std::cout << "\n";
+
+                    // Erase previous clusters for update
+                    previous_clusters.erase(previous_clusters.begin(), previous_clusters.end());
+*/
         im_msg = cv_bridge::CvImage(std_msgs::Header(), "rgb8", im).toImageMsg();
         pubIm.publish(im_msg);
     }
     catch (cv_bridge::Exception& e){
         ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
     }
+/*    
+    std::cout << new_count << " new clusters\n";
 
+    previous_count = new_count; 
+    std::cout << previous_count << " previous clusters\n"; 
 
-    // Count new clusters
-    for (int i=0; i<sizeof(cluster_centers)/sizeof(cluster_centers[0]); i++){
-            for (int j=0; j<=1; j++){
-                    if (cluster_centers[i][j]!=0){
-//                          previous_clusters[i][j]=cluster_centers[i][j];
-                            new_count++;
-                    }
-            }
-    }
-    new_count/=2;
-
-   // Save previous clusters
-   for (int i=1; i<=previous_count; i++){
-        for (int j=0; j<=1; j++){
-                previous_clusters[i][j]=cluster_centers[i][j];
-        }
-   }
-
-        //Update command window
-//    std::cout << "-----------------<<<<>>>>-------------------\n";
-    std::cout << "New number of clusters is " << new_count << "\n";
-    std::cout << "Previous number of clusters was "<< previous_count << "\n";
-
-    // Compare previous with new
-    if (new_count>previous_count){
-           dif=new_count-previous_count;
-           std::cout << dif <<" new clusters created\n";
-    }
-    if (new_count<previous_count){
-           dif=previous_count-new_count;
-           std::cout << dif <<" clusters merged or deleted\n";
-    }
-
-    // Show previous clusters
-    std::cout << "                              PREVIOUS CLUSTERS..\n";
-    for (int i=1; i<=previous_count; i++){
-        std::cout << "                          ID:"<< i << "   Center:";
-         for (int j=0; j<=1; j++){
-                if (previous_clusters[i][j]!=0) {
-                        std::cout << previous_clusters[i][j];
-                        if (j==0) {std::cout << ",";}
-                        std::cout << " ";
-                }
-          }
-          std::cout << "\n";
-    }
-
-    previous_count=new_count;
-    std::cout << "-----------------<<<<>>>>-------------------\n";
-
+    //ERASE PREVIOUS CLUSTERS FOR UPDATE
+    previous_clusters.erase(previous_clusters.begin(), previous_clusters.end());
+*/
 }
 
 void eventCallback(const dvs_msgs::EventArray::ConstPtr &msg){
@@ -153,4 +156,17 @@ int main(int argc, char** argv)
     nh.getParam("kappa", kappa); 
     nh.getParam("alpha", alpha);
     nh.getParam("minN", minN);
+
+
+    eclustering->init(szBuffer, radius, kappa, alpha, minN);
+
+    ros::Subscriber subEv = nh.subscribe("/dvs/events", 1, eventCallback);
+
+    image_transport::ImageTransport it(nh_public);
+    pubIm = it.advertise("clusters_image", 1);
+    image_transport::Subscriber subIm = it.subscribe("dvs/image_raw", 1, imageCallback);
+
+
+    ros::spin();
+
 }

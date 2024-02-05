e17cf0c84b9c4458e017598f7b632b35478ff4a2
Ioannis Machaliotis <johnmax1996@gmail.com>
2024-02-05 16:33:23 +0100

    Commit old changes to check if there is an upstream linkage with local

--- src/AEClustering.cpp
+++ src/AEClustering.cpp
@@ -1,8 +1,13 @@
-#include "grvc_e_clustering/AEClustering.h"
+#include "clustering_with_ids/AEClustering.h"
 
-AEClustering::AEClustering(){
+// ---- Libraries Added ----
+#include "stdio.h"
+#include <iostream>
+
+AEClustering::AEClustering()
+{
     minN_ = 5;
-    szBuffer_ = 0;
+    szBuffer_ = 800;
     tMin_ = 0;
     radius_ = 25;
     alpha_ = 0.8;
@@ -12,7 +17,8 @@ AEClustering::AEClustering(){
     eventId_ = 0;
 }
 
-void AEClustering::init(int szBuffer, double radius, int kappa, double alpha, int minN){
+void AEClustering::init(int szBuffer, double radius, int kappa, double alpha, int minN)
+{
     szBuffer_ = szBuffer;
     radius_ = radius;
     alpha_ = alpha;
@@ -20,13 +26,15 @@ void AEClustering::init(int szBuffer, double radius, int kappa, double alpha, in
     kappa_ = kappa;
 }
 
-bool AEClustering::update(const std::deque<double> e){
-    if(t0_ < 0){
+bool AEClustering::update(const std::deque<double> e)
+{
+    if (t0_ < 0)
+    {
         t0_ = e[0];
     }
 
     Eigen::VectorXd pix(2);
-    pix(0) = e[1]; 
+    pix(0) = e[1];
     pix(1) = e[2];
 
     t = e[0] - t0_;
@@ -34,62 +42,72 @@ bool AEClustering::update(const std::deque<double> e){
     std::deque<int> assigned;
     std::deque<int> removed;
     updateBuffer_(t);
-   
-    //Evaluate proximity with clusters
-    for(int ii = 0; ii < clusters.size(); ii++){
+
+    // Evaluate proximity with clusters
+    for (int ii = 0; ii < clusters.size(); ii++)
+    {
         clusters[ii].forget(tMin_);
 
-        if (clusters[ii].getN() == 0){
-            // Remove the cluster when it is empty
+        // Added 2nd condition ---> improves performance by refreshing buffer of events used for a cluster
+        if (clusters[ii].getN() == 0 || clusters[ii].getN() > szBuffer_) 
+        {
+            // Remove the cluster when it is empty or when it exceeds buffer_sz
             removed.push_back(ii);
-//	    clusters[ii].reset(alpha_, kappa_, minN_);
         }
-        else if (clusters[ii].manhattanDistance(pix) <= radius_){
+        else if (clusters[ii].manhattanDistance(pix) <= radius_)
+        {
             // Assign to the cluster if it is close to the weighted moving average
             assigned.push_back(ii);
         }
-        else if(clusters[ii].getN() > minN_){
+        else if (clusters[ii].getN() > minN_)
+        {
             // Sampling old points from cluster and assign to the cluster if it is close to any of them
-            if (clusters[ii].manhattanDistanceWithSampling(pix) <= radius_){
+            if (clusters[ii].manhattanDistanceWithSampling(pix) <= radius_)
+            {
                 assigned.push_back(ii);
             }
         }
     }
 
-    //No proximity -> new cluster
-    if(assigned.size() == 0){
-//	clusters[assigned[0]].reset(alpha_, kappa_, minN_);
+    // No proximity -> new cluster
+    if (assigned.size() == 0)
+    {
         clusters.push_back(MyCluster(alpha_, kappa_));
-        clusters[clusters.size()-1].add(e, eventId_, t0_);
-        lastUpdatedCluster_ = clusters.size()-1;
+        clusters[clusters.size() - 1].add(e, eventId_, t0_);
+        lastUpdatedCluster_ = clusters.size() - 1;
     }
-    else{
+    else
+    {
         lastUpdatedCluster_ = assigned[0];
         clusters[assigned[0]].add(e, eventId_, t0_);
 
-        //Proximity to more than one cluster-> merging
-        if(assigned.size() >= 2){
+        // Proximity to more than one cluster-> merging
+        if (assigned.size() >= 2)
+        {
             merge_clusters_(assigned);
-            return false; 
+            return false;
         }
     }
 
-    for(int ii = removed.size()-1; ii >= 0; ii--){
-        if(lastUpdatedCluster_ > removed[ii])
+    for (int ii = removed.size() - 1; ii >= 0; ii--)
+    {
+        if (lastUpdatedCluster_ > removed[ii])
             lastUpdatedCluster_--;
 
-        // delete the cluster
+        // Delete the cluster
         clusters.erase(clusters.begin() + removed[ii]);
     }
 
     return false;
 }
 
-int AEClustering::getLastUpdatedClusterIdx(){
+int AEClustering::getLastUpdatedClusterIdx()
+{
     return lastUpdatedCluster_;
 }
 
-int AEClustering::getMinN(){
+int AEClustering::getMinN()
+{
     return minN_;
 }
 
@@ -97,16 +115,18 @@ int AEClustering::getMinN(){
  *                          PRIVATE                               *
  * ************************************************************** */
 
-void AEClustering::updateBuffer_(double t){
-
+void AEClustering::updateBuffer_(double t)
+{
     tBuffer_.push_back(t);
-    if (tBuffer_.size() > szBuffer_){
+    if (tBuffer_.size() > szBuffer_)
+    {
         tBuffer_.pop_front();
     }
     tMin_ = tBuffer_[0];
 }
 
-void AEClustering::merge_clusters_(std::deque<int> assigned){
+void AEClustering::merge_clusters_(std::deque<int> assigned)
+{
 
     int m = assigned.size();
 
@@ -123,7 +143,8 @@ void AEClustering::merge_clusters_(std::deque<int> assigned){
     std::deque<double> aux_datT;
     std::deque<bool> aux_datPol;
 
-    for(int ii = 0; ii < m; ii++){
+    for (int ii = 0; ii < m; ii++)
+    {
         nn.push_back(clusters[assigned[ii]].getN());
         datId.push_back(clusters[assigned[ii]].getDatId());
         dat.push_back(clusters[assigned[ii]].getDat());
@@ -134,24 +155,30 @@ void AEClustering::merge_clusters_(std::deque<int> assigned){
     }
 
     Eigen::VectorXd aux_mu(Eigen::VectorXd::Zero(2));
-    for(int ii = 0; ii < m; ii++){    
-        aux_mu += ((double) clusters[assigned[ii]].getN() / (double) aux_n) * clusters[assigned[ii]].getMu();
+    for (int ii = 0; ii < m; ii++)
+    {
+        aux_mu += ((double)clusters[assigned[ii]].getN() / (double)aux_n) * clusters[assigned[ii]].getMu();
     }
-    
+
     int idx = 1;
     double tt;
-    while(idx >= 0){
+    while (idx >= 0)
+    {
         idx = -1;
         tt = std::numeric_limits<double>::max();
-        for(int jj = 0; jj < m; jj++){
-            if(count[jj] < nn[jj]){
-                if(datT[jj][count[jj]] < tt){
+        for (int jj = 0; jj < m; jj++)
+        {
+            if (count[jj] < nn[jj])
+            {
+                if (datT[jj][count[jj]] < tt)
+                {
                     idx = jj;
                     tt = datT[jj][count[jj]];
                 }
             }
         }
-        if (idx >= 0){
+        if (idx >= 0)
+        {
             aux_datId.push_back(datId[idx][count[idx]]);
             aux_dat.push_back(dat[idx][count[idx]]);
             aux_datT.push_back(datT[idx][count[idx]]);
@@ -166,17 +193,9 @@ void AEClustering::merge_clusters_(std::deque<int> assigned){
     clusters[assigned[0]].setDatT(aux_datT);
     clusters[assigned[0]].setDatPol(aux_datPol);
     clusters[assigned[0]].setMu(aux_mu);
-//    clusters[assigned[0]].reset(alpha_, kappa_, minN_);
 
-    for(int ii = m-1; ii > 0; ii--){
-        clusters.erase(clusters.begin()+assigned[ii]);
-	//reset clusters
-//	clusters[assigned[ii]].reset(alpha_, kappa_, minN_);
+    for (int ii = m - 1; ii > 0; ii--)
+    {
+        clusters.erase(clusters.begin() + assigned[ii]);
     }
-/*
-    for (int ii = 0; ii < m-1; ii++){
-	clusters[assigned[ii]].reset(alpha_, kappa_, minN_);
-    }
-*/
 }
-
